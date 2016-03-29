b477ead6aa2dcd3c803e64360b77227eb5021843
blackout314 <blackout_lair@yahoo.com>
2016-03-29 22:14:38 +0200

    some improvements and fixes about type

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# Serve Responsive Image
+# Serve Responsive Images
 Little _Server Side_ Drone serve responsive images
 
+## Htaccess
+
+This piece of code use htaccess for simple route
--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
 // -- simgr --
-
 $conf['s3'] = null;
 
-// -----------
 
-$nam = '';
-$get = explode('-_-', addslashes($_GET['r']));
+
+// -----------
+$resource = addslashes($_GET['r']);
+
+$get = explode('-_-', $resource ); 
 $xdi = explode('x', $get[0]);
 $dim = $xdi[0];
+$ext = explode('.', $resource );
 
+
+
+// -- dimension case
 if($dim < 768 ) {
 	$name = 'xs';
 } elseif($dim >= 768 && $dim < 992) {
@@ -21,14 +26,25 @@ if($dim < 768 ) {
 	$name = 'lg';
 }
 
-$file = $name.'_'.$get[1];
+
+
+// -- extension case for header
+$type = strtolower($ext[1]);
+if($type === 'png') {
+  header("Content-type: image/png");
+} elseif($type === 'jpg' || $type === 'jpeg') {
+  header("Content-type: image/jpg");
+}
+
+
+
+$file   = $name.'_'.$get[1];
 $stream = $get[1];
 if(is_file('img/'.$file)) {
 	$stream = $file;
+  echo file_get_contents('img/'.$stream);
 } // else create it
 
-header("Content-type: image/png");
-echo file_get_contents('img/'.$stream);
-die();
 
-?>
+
+die();
